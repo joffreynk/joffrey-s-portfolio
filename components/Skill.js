50e63a814ca01c761f6368da61886f6606062182
JoffreyNK <joyjoffrey@gmail.com>
2022-12-26 01:50:35 +0200

    create skills

--- components/Skill.js
+++ components/Skill.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import {SiRubyonrails } from 'react-icons/si'
+import {TbBrandNextjs } from 'react-icons/tb'
 import {motion} from 'framer-motion'
 
 
 const Skill = ({directionLeft}) => {
+  const  commonStyles = 'w-14 h-14 es:w-18 es:h-18 sm:w-20 sm:h-20 md:w-24 md:h-24  xl:w-32 xl:h-32 rounded-full'
   return (
     <div className='group relative flex cursor-pointer'>
-      <motion.img
+      <motion.div 
       initial={{
         x: directionLeft ? -200: 200,
         opacity: 0,
@@ -13,14 +16,12 @@ const Skill = ({directionLeft}) => {
 
       transition={{duration: 1}}
       whileInView={{opacity: 1, x:0}}
-      src='/technologies/css3.png'
-      width={50}
-      height={50}
-      alt='Skill'
-      className='rounded-full border border-gray-500 object-cover w-16 h-16 md:w-24 md:h-24  xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
-      />
 
-      <div className='absolute opacity-0 group-hover:opacity-60 transition duration-300 ease-in-out w-16 h-16 md:w-24 md:h-24  xl:w-32 xl:h-32 rounded-full group-hover:bg-white'>
+      className={`uppercase border flex justify-center items-center text-6xl border-gray-500 object-cover filter group-hover:grayscale transition duration-300 ease-in-out ${commonStyles}`}>
+        <TbBrandNextjs className='w-full h-full' />
+      </motion.div>
+
+      <div className={`absolute opacity-0 group-hover:opacity-60 transition duration-300 ease-in-out  group-hover:bg-white ${commonStyles}`}>
         <div className='flex items-center justify-center h-full'>
           <p className='text-2xl font-bold text-black opacity-60 '>100</p>
         </div>
