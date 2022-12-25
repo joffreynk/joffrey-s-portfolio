83ecf61b33a6326331ce149cb5d6bda73e42ae8f
JoffreyNK <joyjoffrey@gmail.com>
2022-12-26 00:37:09 +0200

    add new break break points for media devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,26 @@ module.exports = {
   content: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {},
+    screens: {
+      'xs': '360px',
+      // => @media (min-width: 360px) { ... }
+      'es': '370px',
+      // => @media (min-width: 360px) { ... }
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
