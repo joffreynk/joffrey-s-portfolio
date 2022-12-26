e49896561715ba96f7e09ad3228652d32c4f8d4e
JoffreyNK <joyjoffrey@gmail.com>
2022-12-26 23:37:14 +0200

    add stars sparator to testimonials

--- components/TestimonailCard.js
+++ components/TestimonailCard.js
@@ -1,5 +1,6 @@
 import {motion} from 'framer-motion';
 import { SocialIcon } from 'react-social-icons'
+import {MdOutlineStars} from 'react-icons/md'
 import Image from 'next/image'
 
 import styles from './styles.module.css'
@@ -31,19 +32,28 @@ const TestimonailCard = ({person}) => {
         className='h-32 w-32 rounded-full xl:w-[120px] xl:h-[120px] object-cover object-center'
         />
       </motion.div>
-      <div className='px-0 md:px-10'>
-        <h4 className='text-lg md:text-xl xl:text-2xl font-light  text-gray-400'>{person.name}</h4>
-      </div>
-      <p className='text-base text-gray-400 px-6' >
+      
+      <p className='text-base text-gray-400 px-6 text-justify' >
       {person.quote}
       </p>
-      <div className='self-end flex justify-center items-center gap-0'>
-      <p className='text-2xl m-0 p-0'>visit:</p>
-      <SocialIcon
+      <div className='flex'>
+        <MdOutlineStars className='text-blue-500' />
+        <MdOutlineStars  className='text-blue-500' />
+        <MdOutlineStars  className='text-blue-500' />
+        <MdOutlineStars  className='text-blue-500' />
+        <MdOutlineStars  className='text-blue-500' />
+        <MdOutlineStars  className='text-blue-500' />
+      </div>
+
+      <div className='px-0 md:px-10'>
+        <h4 className='text-lg md:text-xl xl:text-2xl font-light  text-gray-400'>{person.name}: <SocialIcon
           url={person.url}
           fgColor='blue'
           bgColor='transparent'
-        /> 
+        /> </h4>
+      </div>
+      <div className='px-0 md:px-10'>
+        <p className='text-base font-light  text-gray-400'>{person.position}</p>
       </div>
     </article>
   )
--- components/Testimonials.js
+++ components/Testimonials.js
@@ -8,24 +8,28 @@ const Testimonials = () => {
     
     {
       name:'Dario Alession',
+      position:'Mechanical Engineer, MBA, software developer',
       url: "https://www.linkedin.com/in/dario-alessio",
       picture:'/testimonials/dario.jpeg',
       quote: "Joffrey is one of the finest programmers I have met. Years of study and practical experience combined with a character focused on problem-solving and getting the job done sense. On top of that, a great team member with excellent communication skills, and a great patient in teaching and helping junior programmers or teammates when stuck. He's also got the great talent to deal with the pressure of deadlines. If your company lives under that kind of pressure, he's certainly the right guy to hire."
     },
     {
       name:'David Vera Castillo',
+      position:'Full-Stack Developer, Enterpreneur',
       url: "https://www.linkedin.com/in/davidveracastillo",
       picture:'/testimonials/david.jpeg',
       quote: "Joffrey is a remarkable developer. I know him because we worked together during the first weeks in Microverse, which were the most challenging. He is a reliable partner and skilled programmer. We were able to develop projects with tight deadlines, and he always had a great learning attitude while being generous with his knowledge. Our team benefitted a lot from his contributions when pair programming. I'm sure he will be an asset for any team he's on!"
     },
     {
       name:'Hans Zizold',
+      position:'Telecommunication Engineer and full-stack developer',
       url: "https://www.linkedin.com/in/hanszizold",
       picture:'/testimonials/hans.jpeg',
       quote:"Joffrey is one of the best coding partners I had the luck to work with. He always had a clear idea of the projects we had to do and implemented them with some short elegant lines of code. He also took his time to help and encourage others to succeed."
     },
     {
       name:'Elson Otake',
+      position:'Backend Developer, Code Reviewer',
       url: "https://www.linkedin.com/in/elsonotake",
       picture:'/testimonials/elson.jpeg',
       quote: "Joffrey is a fantastic software engineer. In some chats we usually did, he taught several concepts related to React, presenting all his technical knowledge. If you need to get a job done efficiently, Joffrey is the one for you."
