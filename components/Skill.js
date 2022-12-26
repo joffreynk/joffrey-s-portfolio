f953c1fbea79e4536152203732f26b59b737f6cc
JoffreyNK <joyjoffrey@gmail.com>
2022-12-26 08:46:10 +0200

    add margin to the top

--- components/Skill.js
+++ components/Skill.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import {SiRubyonrails } from 'react-icons/si'
+
 import {TbBrandNextjs } from 'react-icons/tb'
 import {motion} from 'framer-motion'
-
 
 const Skill = ({directionLeft}) => {
   const  commonStyles = 'w-14 h-14 es:w-18 es:h-18 sm:w-20 sm:h-20 md:w-24 md:h-24  xl:w-32 xl:h-32 rounded-full'
@@ -21,7 +20,7 @@ const Skill = ({directionLeft}) => {
         <TbBrandNextjs className='w-full h-full' />
       </motion.div>
 
-      <div className={`absolute opacity-0 group-hover:opacity-60 transition duration-300 ease-in-out  group-hover:bg-white ${commonStyles}`}>
+      <div className={`absolute opacity-0 group-hover:opacity-60 transition duration-300 ease-in-out  group-hover:bg-white ${commonStyles} z-10`}>
         <div className='flex items-center justify-center h-full'>
           <p className='text-2xl font-bold text-black opacity-60 '>100</p>
         </div>
--- components/Skills.js
+++ components/Skills.js
@@ -13,7 +13,7 @@ const Skills = () => {
     className='h-fit relative flex flex-col overflow-hidden text-left md:flex-row  px-4 sm:px-10 justify-evenly mx-auto items-center'>
       <h3 className={`absolute top-20 font-bold ${styles.headertext} uppercase tracking-[10px] es:tracking-[15px] sm:tracking-[20px] text-gray-500 text-4xl`}>skills </h3>
       <h3 className='absolute top-44 uppercase tracking-[3px] text-gray-500 text-sm'>Hover over for proficiency</h3>
-      <div className='grid grid-cols-3 md:grid-cols-4 mt-36 mb-14 gap-8 md:gap-10 snap-x snap-mandatory'>
+      <div className='grid grid-cols-3 md:grid-cols-4 mb-14 gap-8 md:gap-10 snap-x snap-mandatory mt-52'>
         <Skill />
         <Skill />
         <Skill />
@@ -32,7 +32,7 @@ const Skills = () => {
         <Skill />
 
       </div>
-      <div className='w-full absolute top-[37%] bg-[#f7ab8a]/10 ltf-0 h-[400px] skew-y-12' />
+      <div className='w-full absolute top-[37%] bg-[#f7ab8a]/10 ltf-0 h-[400px] skew-y-12 z-0' />
     </motion.div>
   )
 }
