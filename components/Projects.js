5ce38c003e40e4e6c95a8018437461723f942556
JoffreyNK <joyjoffrey@gmail.com>
2022-12-26 09:25:11 +0200

    add projects cards

--- components/Projects.js
+++ components/Projects.js
@@ -8,9 +8,9 @@ const Projects = () => {
     <div
     className='h-fit relative flex flex-col overflow-hidden text-left md:flex-row max-w-4/5 justify-evenly mx-auto items-center z-0 '>
       <h3 className={`absolute top-20 font-bold ${styles.headertext} uppercase tracking-[10px] es:tracking-[15px] sm:tracking-[20px] text-gray-500 text-4xl`}>projects</h3>
-      <div className='relative w-full flex overflow-y-hidden snap-x snap-mandatory z-20 mt-16'>
+      <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-10 p-14 relative snap-x snap-mandatory mt-44 z-20'>
         {projects.map((project, i)=>{
-          return <div key={i+1} className='w-screen md:w-1/2 flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-5 md:p-32 h-screen'>
+          return <div key={i+1} className={`flex-shrink-0 snap-center flex flex-col items-center justify-center py-6 px-3 ${styles.expericenceBorder}`}>
             <motion.img
             initial={{
               y:-300,
@@ -23,17 +23,22 @@ const Projects = () => {
             alt="project" 
             className='w-60'
             />
-            <div className='space-y-2 md:space-y-10 px-0 md:px-10 max-w-4xl'>
-              <h4 className='text-2xl md:text-4xl font-semibold text-center '>Books store</h4>
-              <p className='text-lg text-left'>
+            <div className='p-4 max-w-4xl'>
+              <h4 className='text-2xl sm:text-4xl md:text-4xl font-semibold text-center py-6 '>Books store</h4>
+              <p className='text-xl sm:text-2xl text-justify space-x-1 break-all'>
                 React-Redux book store application is an application that helps libraries to manage their books in the store. A librarian can add or remove the book from the store. a teacher who is using that book can track the progress of the book and what he has already taught
                 </p>
             </div>
-
+              <div className='flex justify-between w-full'>
+                <button className=''>live</button>
+                <button className=''>Demo</button>
+              </div>
           </div>
+          
         })}
+
       </div>
-      <div className='w-full absolute top-[37%] bg-[#f7ab8a]/10 ltf-0 h-[400px] -skew-y-12' />
+      <div className='w-full absolute top-[37%] bg-[#f7ab8a]/10 ltf-0 h-[400px] -skew-y-12 z-0' />
       </div>
   )
 }
