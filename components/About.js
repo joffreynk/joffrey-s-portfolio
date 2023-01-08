1f31172ab8adb43de4b97e2984767413d8d05205
Joffreynk <joyjoffrey@gmail.com>
2023-01-08 11:34:09 +0200

    Change about Header

--- components/About.js
+++ components/About.js
@@ -8,7 +8,7 @@ const About = () => {
   return (
     <div className="mb-16  min-h-fit flex flex-col relative text-center md:text-left md:flex-row max-w-7xl justify-evenly px-10 mx-auto items-center">
 
-      <h3 className={`absolute top-16 es:top-20 font-bold ${styles.headertext} uppercase tracking-[10px] es:tracking-[15px] sm:tracking-[20px] text-gray-500 text-4xl`}>About</h3>
+      <h3 className={`absolute top-16 es:top-20 font-bold ${styles.headertext} uppercase tracking-[10px] es:tracking-[15px] sm:tracking-[20px] text-gray-500 text-4xl`}>About Me</h3>
     
     <div className='mt-44  xs:top-32  flex text-justify flex-col md:flex-row max-w-7xl justify-center mx-2 items-center' >
      
@@ -90,7 +90,7 @@ const About = () => {
           
           className="space-y-2 es:space-x-10 px-0 md:px-10 flex-grow xs:w-full">
             <h5 className='text-2xl mt-3 font-semibold text-center  text-gray-400 underline pb-5 decoration-[#8c61c5] '>
-              Little background
+              Biography
             </h5>
             <p className='mt-0 pt-0 text-[18px]  text-gray-400  '>
             Hi, my name is Joffrey NKESHIMANA, I have a bachelor&#39;s degree in computer science. I&#39;m a Ruby on Rails and NextJS full-stack developer with the mission of enhancing technology by turning ideas into real-world applications.
