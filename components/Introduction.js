40db3d7bb80fe35b1b4476c380a3f0fae1559bdd
JoffreyNK <joyjoffrey@gmail.com>
2023-01-09 17:03:57 +0200

    add

--- components/Introduction.js
+++ components/Introduction.js
@@ -23,34 +23,34 @@ const Introduction = () => {
       ? 
       <button onClick={()=>setDisplayMenu(!displayMenu)}><BiMenu className="text-4xl bg-slate-300 rounded-md text-[rgb(36,36,36)] self-start " /></button> 
       :
-      <div className=" fixed z-50 top-0 left-0 right-0 bottom-0 text-lg  bg-slate-300 bg-opacity-70 ">
-        <div className=" fixed top-20 left-3 right-1/4 bottom-0  flex justify-between items-start text-gray-500  bg-slate-300 p-5 bg-opacity-90">
+      <div className=" fixed z-50 top-0 left-0 right-0 bottom-0 text-lg bg-[rgb(36,36,36)]  text-gray-500 bg-opacity-60 ">
+        <div className=" fixed top-20 left-3 right-1/4 bottom-0  flex justify-between items-start text-gray-500 bg-[rgb(36,36,36)] p-5 bg-opacity-90">
           <div className="text-3xl flex flex-col gap-5 ">
-          <Link onClick={()=> setDisplayMenu(!displayMenu)} href="#home" className="flex gap-3 items-center border-b-2 w-fit hover:bg-[#8f8989] p-2 rounded-md ">
+          <Link onClick={()=> setDisplayMenu(!displayMenu)} href="#home" className="flex gap-3 items-center border-b-2 w-fit p-2 rounded-md ">
             <AiOutlineHome className=" text-violet-400" />
             <button className={`introButtons  capitalize  hover:scale-x-75 duration-125`}>Home</button>
           </Link>
-          <Link onClick={()=> setDisplayMenu(!displayMenu)} href="#about" className="flex gap-3  items-center border-b-2 w-fit hover:bg-[#8f8989] p-2 rounded-md ">
+          <Link onClick={()=> setDisplayMenu(!displayMenu)} href="#about" className="flex gap-3  items-center border-b-2 w-fit p-2 rounded-md ">
           <FcReading className="text-violet-400" />
             <button className={`introButtons capitalize hover:scale-x-75 duration-125 `}>about</button>
           </Link>
-          <Link onClick={()=> setDisplayMenu(!displayMenu)} href="#experience" className="flex gap-3 items-center border-b-2 w-fit hover:bg-[#8f8989] p-2 rounded-md ">
+          <Link onClick={()=> setDisplayMenu(!displayMenu)} href="#experience" className="flex gap-3 items-center border-b-2 w-fit p-2 rounded-md ">
           <GiAchievement className="text-violet-400" />
             <button className={`introButtons capitalize hover:scale-x-75 duration-125 `}>experience</button>
           </Link>
-          <Link onClick={()=> setDisplayMenu(!displayMenu)} href="#skills" className="flex gap-5  items-center border-b-2 w-fit hover:bg-[#8f8989] p-2 rounded-md ">
+          <Link onClick={()=> setDisplayMenu(!displayMenu)} href="#skills" className="flex gap-5  items-center border-b-2 w-fit p-2 rounded-md ">
           <GiSkills className="text-violet-400" />
             <button className={`introButtons capitalize hover:scale-x-75 duration-125 `}>skills</button>
           </Link>
-          <Link onClick={()=> setDisplayMenu(!displayMenu)} href="#projects" className="flex gap-3 items-center border-b-2 w-fit hover:bg-[#8f8989] p-2 rounded-md ">
+          <Link onClick={()=> setDisplayMenu(!displayMenu)} href="#projects" className="flex gap-3 items-center border-b-2 w-fit p-2 rounded-md ">
           <AiOutlineFundProjectionScreen className="text-violet-400" />
             <button className="introButtons capitalize hover:scale-x-75 duration-125 ">projects</button>
           </Link>
-          <Link onClick={()=> setDisplayMenu(!displayMenu)} href="#testimonials" className="flex gap-3 items-center border-b-2 w-fit hover:bg-[#8f8989] p-2 rounded-md ">
+          <Link onClick={()=> setDisplayMenu(!displayMenu)} href="#testimonials" className="flex gap-3 items-center border-b-2 w-fit p-2 rounded-md ">
           <MdOutlinePreview className="text-violet-400" />
             <button className="introButtons capitalize hover:scale-x-75 duration-125 ">Testimonials</button>
           </Link>
-          <Link onClick={()=> setDisplayMenu(!displayMenu)} href="#contacts" className="flex gap-3 items-center border-b-2 w-fit hover:bg-[#8f8989] p-2 rounded-md ">
+          <Link onClick={()=> setDisplayMenu(!displayMenu)} href="#contacts" className="flex gap-3 items-center border-b-2 w-fit p-2 rounded-md ">
           <MdContactMail className="text-violet-400" />
             <button className="introButtons capitalize hover:scale-x-75 duration-125 ">Contacts</button>
           </Link>
